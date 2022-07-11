bb21dc202b5a47eb01e5236adf3126693f2de856
asmeyb <asmeyb@gmail.com>
2022-07-11 21:29:35 +0300

    About Page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,10 @@
 import React from "react";
-import asmamaw from "../imgaes/asme.jpg";
+import asmamaw from "../images/asme.jpg";
+import abebe from "../images/abebe.jpg";
 
 const About = () => {
 	return (
-		<div>
+		<div className="container">
 			<p className="lead mt-10">
 				Blogs are much appreciated across the internet. Blogs can be very
 				valuable for businesses. Personal blogs have been made like a million
@@ -13,30 +14,22 @@ const About = () => {
 				that is a lot more valuable to businesses that want their blog to grow
 				organically and bring in traffic.
 			</p>
-			<div class="row colspan-10">
-				<div class="col-sm-6">
+			<div class="row">
+				<div class="col-12 col-md-6 col-lg-6">
 					<div class="card">
-						<div class="card-body">
-							<h5 class="card-title">Asmamaw Yismaw</h5>
-							<p class="card-text">
-								Some Text About Asmamaw Yismaw
-							</p>
-							<a href="#" class="btn btn-primary">
-								Read more
-							</a>
+						<img src={asmamaw} className="card-img-top" />
+						<div className="card-body">
+							<h5 className="card-title">Asmamaw Yismaw</h5>
+							<p className="card-text">MERN Full Stack Developer</p>
 						</div>
 					</div>
 				</div>
-				<div class="col-sm-6">
+				<div class="col-12 col-md-6 col-lg-6">
 					<div class="card">
-						<div class="card-body">
-							<h5 class="card-title">Abebe Tesfaye</h5>
-							<p class="card-text">
-                            Some Text About Abebe Tesfaye
-							</p>
-							<a href="#" class="btn btn-primary">
-								Read more
-							</a>
+						<img src={abebe} className="card-img-top img-circle" />
+						<div className="card-body">
+							<h5 className="card-title">Abebe Tesfaye</h5>
+							<p className="card-text">MERN Full Stack Developer</p>
 						</div>
 					</div>
 				</div>
