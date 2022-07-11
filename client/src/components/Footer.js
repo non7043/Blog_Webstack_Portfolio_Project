9d5bb5e53836193d595394eff244c25eb3234450
non7043 <non7043@gmail.com>
2022-07-11 15:38:29 +0300

    changes made to Footer, nav,signinform and about

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <p>&copy; {new Date().getFullYear()} Made with ❤️ by Jakaria Hridoy</p>
+        <p>&copy; {new Date().getFullYear()} Made with ❤️</p>
     )
 }
 
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,7 +3,12 @@ import React from 'react';
 
 const About = () =>  {
     return (
-        <h1>This is About page!</h1>
+        <h1>
+            Blogs are much appreciated across the internet. Blogs can be very valuable for businesses. 
+            Personal blogs have been made like a million times by Web Devs already, but user-generated blogs that have a built-in markdown editor,
+            review algorithm and everything else needed to have a self-sustaining blog is something not many have made, 
+            yet is something that is a lot more valuable to businesses that want their blog to grow organically and bring in traffic. 
+        </h1>
     )
 }
 
