0629f3b7d495e6c35bfa4957aa9b4b2cd1ad29d3
non7043 <non7043@gmail.com>
2022-07-12 12:46:10 +0300

    change to keys

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-rh5ea.mongodb.net/?retryWrites=true&w=majority`
+    mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-rh5ea.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 }
